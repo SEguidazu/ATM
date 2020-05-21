d4af7f00b058ae921d952f562066910c1a64036b
Santiago Eguidazu <eguidazu.san@gmail.com>
2020-05-21 00:34:33 -0300

    Avance en clase Cliente, se añadio una Exception personalizada y el package de excepciones para futuras creaciones. Se agrego el UML v03.svg siendo casi final.

---
+++ exceptions/exceptions/CuitErroneoException.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+public class CuitErroneoException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3395473542730176551L;
+
+	public CuitErroneoException(String s) {
+		super(s);
+	}
+}
--- src/atm/Cliente.java
+++ src/atm/Cliente.java
@@ -1,6 +1,9 @@
 package atm;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import exceptions.CuitErroneoException;
 
 public class Cliente {
 	
@@ -9,12 +12,28 @@ public class Cliente {
 	private List<Cuenta> cuentas;
 	
 	
-	public Cliente(String alias, String CUIT) {
+	public Cliente(String alias, String CUIT) throws CuitErroneoException {
 		
-		
+		cuentas = new ArrayList<Cuenta>();
+		setCUIT(CUIT);
 	}
 	
 	
+	private void setCUIT(String CUIT) throws CuitErroneoException {
+
+		if(!sonNumeros(CUIT)) {
+			
+			throw new CuitErroneoException("El CUIT se compone solo de números.");
+		} else if(CUIT.length() == 11) {
+			
+			throw new CuitErroneoException("El CUIT se compone de 11 números.");
+		} else {
+		
+			this.CUIT = CUIT;
+		}
+	}
+
+
 	public String getAlias() {
 		
 		return alias;
@@ -29,12 +48,23 @@ public class Cliente {
 	
 	public void agregarCuenta(Cuenta cuenta) {
 		
-		
+		cuentas.add(cuenta);
 	}
 	
 	
 	public List<Cuenta> obtenerCuentas() {
 		
-		return null;
+		return cuentas;
+	}
+
+
+	private boolean sonNumeros(String cadena){
+		
+		try {
+			Integer.parseInt(cadena);
+			return true;
+		} catch (NumberFormatException nfe){
+			return false;
+		}
 	}
 }
